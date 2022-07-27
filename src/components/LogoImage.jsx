11600d892c27399470dc3ec616f781e6d51698c9
Laura Arango <lmarangoc@gmail.com>
2022-07-26 19:06:25 -0500

    tailwind and login

---
+++ src/components/LogoImage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Logo from '../public/logo192.png';
+
+const ImagenLogo = () => {
+  return <img className='mx-auto h-40 w-auto' src={Logo} alt='COVID-19 Tracker' />;
+};
+
+export default ImagenLogo;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Photo from '../media/medicine1.png';
+import Logo from '../media/logo.png';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex'>
+      <div className={`w-96 h-screen`}>
+        <div className='p-7 flex flex-col '>
+          <img className='mx-auto h-11 w-auto' src={Logo} alt='COVID-19 Tracker' />
+          <h1> COVID-19 Tracker </h1>
+          <h1> Log in to your account </h1>
+        </div>
+      </div>
+      <div className='p-0 text-2xl font-semibold flex-1 h-screen'>
+        <img className='mx-auto w-full h-full' src={Photo} alt='Medicine' />
+      </div>
+    </div>
   )
 }
 
