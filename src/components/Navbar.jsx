370a1c540cced38056caafed67cc89b5fe1c9eb0
Laura Arango <lmarangoc@gmail.com>
2022-07-28 18:21:35 -0500

    feat: navbar home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,18 @@ import Logo from '../media/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className='bg-indigo-500 px-2 py-2.5'>
-      <div className='container flex flex-wrap justify-between items-center mx-auto'>
+    <nav className='px-2 py-2.5'>
+      <div className='container flex flex-wrap items-center justify-around mx-auto'>
         <Link to='/'>
           <img className='mr-3 h-6 sm:h-9 select-none' src={Logo} alt='COVID-19 Tracker' />
         </Link>
         <div>
           <ul>
-            <li className='block py-2 pr-4 pl-3 text-white rounded-lg shadow-md hover:bg-blue-900'>
-              <Link to='/login' className='select-none'>
-                Sign In
+            <li>
+              <Link to='/login'>
+                <button className='bg-indigo-500 text-white w-full px-4 py-2 rounded hover:bg-indigo-700 select-none'>
+                  Sign In
+                </button>
               </Link>
             </li>
           </ul>
