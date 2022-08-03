9fe0af2b7ba9dcaf8121d8df3d9cbcf4bd1f17f7
Laura Arango <lmarangoc@gmail.com>
2022-08-03 15:35:26 -0500

    chore: components, context and hooks

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import { useUser } from 'context/userContext';
+import React from 'react';
+
+const PrivateRoute = ({ roleList, children }) => {
+  const { userData } = useUser();
+
+  if (roleList.includes(userData.rol)) {
+    return children;
+  }
+
+  return <div className='text-9xl text-red-500 '>You are not authorized to view this site.</div>;
+};
+
+export default PrivateRoute;
