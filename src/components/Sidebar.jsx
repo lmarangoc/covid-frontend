fd17a553f1aef34b14caac1f5389dc300d315682
Laura Arango <lmarangoc@gmail.com>
2022-08-05 21:17:28 -0500

    feat: sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Control from 'media/control.png'
+import Logo from 'media/logo.png'
+import { Link } from "react-router-dom"
+import { nanoid } from 'nanoid'
 
-const Sidebar = () => {
+const Sidebar = ({ menus }) => {
+
+  const[open, setOpen] = useState(true)
+
   return (
-    <div>Sidebar</div>
+    <div className={`${open ? 'w-72' : 'w-20'} duration-300 h-screen p-5 pt-8 bg-indigo-500 relative`}>
+      <img 
+        src={Control} 
+        alt="Responsive Sidebar" 
+        className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-indigo-500 ${!open && 'rotate-180'}`}
+        onClick={() => setOpen(!open)}
+      />
+      <div className='flex gap-x-4 items-center'>
+        <Link to='/' className='flex gap-x-4 items-center'>
+          <img 
+            src={Logo} 
+            alt='Logo'
+            className='h-10 duration-500 select-none'
+          />
+          <h1 className={`text-white origin-left font-medium text-xl duration-300 select-none ${!open && 'scale-0'}`}> Home </h1>
+        </Link>
+      </div>
+      <ul className='pt-6'>
+        {menus.map((menu, index) => (
+          <Link to={menu.route} key={nanoid()}>
+            <li
+            key={index}
+            className={`flex rounded-md p-2 hover:bg-light-white text-gray-100 text-sm items-center gap-x-4 
+            ${menu.gap ? "mt-12" : "mt-5"} ${ index === 0 && "bg-light-white"}`}
+            > 
+              <i className={`${menu.icon}`}/>
+              <span className={`${!open && "hidden"} origin-left duration-200 select-none`}>
+                {menu.title}
+              </span>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
   )
 }
 
