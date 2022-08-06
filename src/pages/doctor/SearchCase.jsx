12d384080b5bb857e58477f3eca84652f8a682a0
Laura Arango <lmarangoc@gmail.com>
2022-08-05 21:36:55 -0500

    feat: add sidebar menu

--- src/pages/doctor/SearchCase.jsx
+++ src/pages/doctor/SearchCase.jsx
@@ -12,7 +12,7 @@ const SearchCase = () => {
 
   return (
     <div className='flex'>
-      <Sidebar/>
+      <Sidebar menus={Menus}/>
       <div className='p-7 text-2xl font-semibold flex-1 h-screen'>
         <h1> Search Case </h1>
       </div>
