a0982ef638b34d5066c8b7bcc9165da7f504ee09
Laura Arango <lmarangoc@gmail.com>
2022-08-16 15:27:32 -0500

    fix: outline

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,10 +17,10 @@ const Login = () => {
       <div className='pt-12 flex justify-center justify-items-center'>
         <form className='shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-gray-300'>
           <div className='border-b border-indigo-500 py-2 mb-4'>
-            <input type="text" className='appearance-none bg-transparent border-none w-full text-gray-900 mr-3 py-1 px-2 leading-tight focus:outline-none' placeholder="username" name="username" required/>
+            <input type="text" className='appearance-none bg-transparent border-none w-full text-gray-900 mr-3 focus:border-transparent focus:ring-0' placeholder="username" name="username" required/>
           </div>
           <div className='border-b border-indigo-500 py-2 mb-8'>
-            <input type="password" className='appearance-none bg-transparent border-none w-full text-gray-900 mr-3 py-1 px-2 leading-tight focus:outline-none' placeholder="password" name="password" required />
+            <input type="password" className='appearance-none bg-transparent border-none w-full text-gray-900 mr-3 focus:border-transparent focus:ring-0' placeholder="password" name="password" required />
           </div>
           <div className='flex justify-center justify-items-center'>
             <Link to='/admin' className='w-full'>
