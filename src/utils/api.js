a726c0bbb9fa6086631f44ea50835c21c748b2aa
Laura Arango <lmarangoc@gmail.com>
2022-08-17 18:21:53 -0500

    feat: helper

--- src/utils/api.js
+++ src/utils/api.js
@@ -44,3 +44,50 @@ export const deleteUser = async (id, successCallback, errorCallback) => {
   };
   await axios.request(options).then(successCallback).catch(errorCallback);
 };
+
+//
+
+export const getCases = async (successCallback, errorCallback) => {
+  const options = {
+    method: "GET",
+    url: "http://localhost:5000/helper/managecase/",
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const getCaseData = async (successCallback, errorCallback) => {
+  const options = {
+    method: "GET",
+    url: "http://localhost:5000/helper/managecase/self",
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const createCase = async (data, successCallback, errorCallback) => {
+  const options = {
+    method: "POST",
+    url: "http://localhost:5000/helper/registercase/",
+    headers: { "Content-Type": "application/json" },
+    data,
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const editCase = async (id, data, successCallback, errorCallback) => {
+  const options = {
+    method: "PATCH",
+    url: `http://localhost:5000/helper/managecase/${id}/`,
+    headers: { "Content-Type": "application/json" },
+    data,
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const deleteCase = async (id, successCallback, errorCallback) => {
+  const options = {
+    method: "DELETE",
+    url: `http://localhost:5000/helper/managecase/${id}/`,
+    headers: { "Content-Type": "application/json" },
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
