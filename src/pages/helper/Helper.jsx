d1b94cf2a99c9fe8bf4eeb48425a8cdaf85931ec
Laura Arango <lmarangoc@gmail.com>
2022-08-18 02:11:10 -0500

    feat: helper page

--- src/pages/helper/Helper.jsx
+++ src/pages/helper/Helper.jsx
@@ -47,29 +47,44 @@ const Helper = () => {
   }, [search, cases])
 
   return (
-    <div>
+    <div className='w-full h-full'>
       <Navbar menus={Menus}/>
-      <div className='px-10 py-32 flex-1 h-screen'>
-        <div className='flex flex-wrap mb-10'>
-          <input 
-            type="search" 
-            placeholder="Search" 
-            value={search} 
-            onChange={(e) => setSearch(e.target.value)} 
-            className='w-4/12 border border-gray-500 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
-          />
-        </div>
-        <div className='flex flex-wrap'>
-          {filterC.map((cases) => {
-            return (
-              <div key={nanoid()} className='p-4 sm:w-1/2 lg:w-1/3'>
-                <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
-                  <img src="https://avatars.dicebear.com/api/bottts/:seed.svg" alt="" className='w-1/2'/>
-                </div>
+      <div className='flex items-center justify-center pt-28'>
+        <input 
+          type="search" 
+          placeholder="Search" 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)} 
+          className='w-4/12 border border-gray-500 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
+        />
+      </div>
+      <div className='flex flex-wrap px-8 py-5'>
+        {filterC.map((cases) => {
+          return (
+            <div key={nanoid()} className='p-4 sm:w-1/2 lg:w-1/3 hover:scale-105'>
+              <div className='h-full border border-gray-200 rounded-lg overflow-hidden'>
+                <p className='text-lg font-semibold text-gray-900 px-4 pt-2 select-none'> 
+                  {cases.name + " " + cases.lastname} 
+                </p>
+                <p className='px-4 select-none'>
+                  {cases.test_result + " " + cases.test_date }
+                </p>
+                <p className='px-4 select-none'>
+                  {"ID Card: " + cases.idcard}
+                </p>
+                <p className='px-4 select-none'>
+                  {"Residence address: " + cases.residence}
+                </p>
+                <p className='px-4 select-none'>
+                  {"Job address: " + cases.job}
+                </p>
+                <p className='px-4 pb-3 select-none'> 
+                  {" Date of birth: " + cases.birth + " Sex: " + cases.sex}
+                </p>
               </div>
-            )
-          })} 
-        </div>
+            </div>
+          )
+        })} 
       </div>
     </div>
   )
