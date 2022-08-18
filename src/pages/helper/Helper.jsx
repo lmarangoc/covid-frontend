12a44a490c567a0ba6d2c89694271c69361ad97b
Laura Arango <lmarangoc@gmail.com>
2022-08-18 00:37:41 -0500

    feat: helper page

--- src/pages/helper/Helper.jsx
+++ src/pages/helper/Helper.jsx
@@ -1,30 +1,75 @@
-import React from 'react'
-import Sidebar from 'components/Sidebar'
-import imgHelper from 'media/helper.png'
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import Navbar from 'components/Navbar'
+import { getCases } from 'utils/api'
+import { nanoid } from 'nanoid'
 
 const Helper = () => {
 
   const Menus = [
-    { title: "Helper", icon: "fa-solid fa-user fa-2x", route: '/helper' },
-    { title: "Register Case", icon: "fa-solid fa-user-plus fa-2x", route: '/helper/registercase', gap: true },
-    { title: "Manage Case", icon: "fa-solid fa-bars-progress fa-2x", route: '/helper/managecase' },
-    { title: "Log Out", icon: "fa-solid fa-arrow-right-from-bracket fa-2x", route: "/", gap: true },
+    { title: "Helper", route: '/helper' },
+    { title: "Register Case", route: '/helper/registercase' },
   ]
 
+  const [cases, setCases] = useState([])
+  const [runQuery, setRunQuery] = useState(false)
+  const [search, setSearch] = useState('')
+  const [filterC, setFilterC] = useState(cases)
+
+  useEffect(() => {
+    const fetchCases = async () => {
+      await getCases(
+        (response) => {
+          console.log('cases', response.data);
+          setCases(response.data)
+          setRunQuery(false)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    }
+    if (runQuery) {
+      fetchCases()
+      setRunQuery(true)
+    }
+  }, [runQuery])
+
+  useEffect(() => {
+    setRunQuery(true)
+  }, [])
+
+  useEffect(() => {
+    setFilterC(
+      cases.filter((elemento) => 
+        JSON.stringify(elemento).toLowerCase().includes(search.toLowerCase()),
+      )
+    )
+  }, [search, cases])
+
   return (
-    <div className='flex'>
-      <Sidebar menus={Menus}/>
-      <div className='container flex flex-col items-center justify-center h-screen'>
-        <div className='flex flex-row mx-auto items-start'>
-          <Link to='/helper/registercase'>
-            <button className='bg-indigo-500 text-white w-32 px-4 py-2 rounded hover:rounded-full select-none mr-10'>Register Case</button>
-          </Link>
-          <Link to='/helper/managecase'>
-            <button className='bg-indigo-500 text-white w-32 px-4 py-2 rounded hover:rounded-full select-none'>Manage Case</button>
-          </Link>
+    <div>
+      <Navbar menus={Menus}/>
+      <div className='px-10 py-32 flex-1 h-screen'>
+        <div className='flex flex-wrap mb-10'>
+          <input 
+            type="search" 
+            placeholder="Search" 
+            value={search} 
+            onChange={(e) => setSearch(e.target.value)} 
+            className='w-4/12 border border-gray-500 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
+          />
         </div>
-        <img className='mx-auto h-4/5 w-auto' src={imgHelper} alt='Helper' />
+        <div className='flex flex-wrap'>
+          {filterC.map((cases) => {
+            return (
+              <div key={nanoid()} className='p-4 sm:w-1/2 lg:w-1/3'>
+                <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
+                  <img src="https://avatars.dicebear.com/api/bottts/:seed.svg" alt="" className='w-1/2'/>
+                </div>
+              </div>
+            )
+          })} 
+        </div>
       </div>
     </div>
   )
